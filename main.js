e9f9ade8f22b735102868f67792de2a61727b58e
Edgar Saucedo <edgarsaucedo60@gmail.com>
2022-08-31 10:47:45 -0500

    heoo

--- main.js
+++ main.js
@@ -1,14 +1,21 @@
+
+//declarcion de variables
 const navEmail = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
 const menuHanIcon = document.querySelector('.menu');
 const mobileMenu = document.querySelector('.mobile-menu');
 const menuCarritoIcon = document.querySelector('.navbar-shopping-cart');
+const productDetailCloseIcon = document.querySelector('.product-detail-close');
+
 const shoppingCartCointainer = document.querySelector('#shoppingCartCointainer');
+const productDetailcontainer = document.querySelector('#productDetail');
 const cardsContainer = document.querySelector('.cards-container')
+
+//Eventos
 navEmail.addEventListener('click',toggleDesktopMenu);
 menuHanIcon.addEventListener('click',toggleMobileMenu);
 menuCarritoIcon.addEventListener('click',toggleCaritoAside);
-
+productDetailCloseIcon.addEventListener('click',closeProductDetailAside);
 function toggleDesktopMenu(){
     
     const isAsideClosed = shoppingCartCointainer.classList.contains('inactive');
@@ -26,6 +33,9 @@ function toggleMobileMenu (){
     if(!isAsideClosed){
         shoppingCartCointainer.classList.add('inactive');
     }
+
+
+    closeProductDetailAside();
     mobileMenu.classList.toggle('inactive');
 
 }
@@ -33,13 +43,33 @@ function toggleMobileMenu (){
 function toggleCaritoAside (){
     const isMobileMenuClosed = mobileMenu.classList.contains('inactive');
     //const isAsideClosed = mobile.classList.contains('inactive');
-    console.log('hola')
+    //console.log('hola')
     if(!isMobileMenuClosed){
         mobileMenu.classList.add('inactive');
     }
+
+    const isProductDetailClosed = productDetailcontainer.classList.contains('inactive');
+    //const isAsideClosed = mobile.classList.contains('inactive');
+    console.log('hola')
+    if(!isProductDetailClosed){
+        productDetailcontainer.classList.add('inactive');
+    }
+
     shoppingCartCointainer.classList.toggle('inactive');
 }
 
+function openProductDetailAsise(){
+    
+    
+    shoppingCartCointainer.classList.add('inactive');
+    productDetailcontainer.classList.remove('inactive');
+
+    
+}
+
+function closeProductDetailAside(){
+    productDetailcontainer.classList.add('inactive');
+}
 const productList = [];
 productList.push({
     name: 'bike',
@@ -85,33 +115,34 @@ function renderProducts(arr){
     
         const productImg = document.createElement('img');
         productImg.setAttribute('src',producto.img);
+        productImg.addEventListener('click',openProductDetailAsise);
     
         const productInfo = document.createElement('div');
         productInfo.classList.add('product-info');
-    
+
         const productInfoDiv = document.createElement('div');
         const productPrice = document.createElement('p');
         productPrice.innerText = "$" + producto.price
         const productName= document.createElement('p');
         productName.innerText = producto.name;
-    
+
         productInfoDiv.appendChild(productPrice);
         productInfoDiv.appendChild(productName);
-    
+
         const productInfoFIgure = document.createElement('figure');
         const productImgCard = document.createElement('img');
         productImgCard.setAttribute('src','./icons/bt_add_to_cart.svg');
-    
+
         productInfoFIgure.appendChild(productImgCard);
-    
+
         productInfoFIgure.appendChild(productImgCard);
-    
+
         productInfo.appendChild(productInfoDiv);
         productInfo.appendChild(productInfoFIgure);
-    
+
         productCard.appendChild(productImg);
         productCard.appendChild(productInfo);
-    
+
         cardsContainer.appendChild(productCard);
     }
 }
